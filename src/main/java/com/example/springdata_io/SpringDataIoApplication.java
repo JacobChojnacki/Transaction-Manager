2ca8192009ebfcd8df69016a8a2716a90d8d1fc7
Jacob Chojnacki <jacobchojnacki@protonmail.com>
2022-04-21 13:52:53 +0200

    Product + ProductRepository

---
+++ src/main/java/com/example/springdata_io/SpringDataIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdata_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataIoApplication.class, args);
+    }
+
+}
